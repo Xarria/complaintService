849d93a4813c0374260fc8d71a10dadf3e504d03
Xarria <224340@edu.p.lodz.pl>
2025-04-14 19:25:50 +0200

    init

---
+++ src/test/java/com/recruitment/task/complaint_service/ComplaintServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.task.complaint_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
